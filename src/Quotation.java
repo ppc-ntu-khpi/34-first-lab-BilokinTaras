1faa44d9a28551275641e243dad9927873a6f07b
BilokinTaras <bilokintaras@gmail.com>
2020-10-13 15:35:17 +0000

    Add classes, quote and author, color shirt

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+  String quote = "“We must not allow other people’s limited perceptions to define us.”";
+  String author ="– Virginia Satir";
+  public void display() {
+    System.out.println(quote);
+    System.out.println(author);
+  }
+}
